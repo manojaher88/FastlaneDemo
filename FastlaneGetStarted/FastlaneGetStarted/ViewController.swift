c72d893950609fb8086dadc7c1106709c932ec4a
Manoj Aher <manojaher88@gmail.com>
2020-04-22 14:07:12 +0530

    Get started with fastlane

---
+++ FastlaneGetStarted/FastlaneGetStarted/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FastlaneGetStarted
+//
+//  Created by admin on 4/22/20.
+//  Copyright © 2020 ManojAher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
